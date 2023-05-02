7585f8d4b8fe9bdcf8400377372bfc02ac78acc9
sharad-bapat <87427801+sharad-bapat@users.noreply.github.com>
2023-05-03 00:34:51 +0530

    Add files via upload

--- swiper.js
+++ swiper.js
@@ -1,5 +1,5 @@
 
-// rss("https://winter-smoke-73a6.sixyjntpqun7805.workers.dev/?url=http://www.engadget.com%2Frss-full.xml").then(data => { items = data.items; showItem(0) });
+// rss("https://winter-smoke-73a6.sixyjntpqun7805.workers.dev/?url=http://www.engadget.com/rss-full.xml").then(data => { items = data.items; showItem(0) });
 let currentIndex = 0
 let items = [];
 
@@ -39,135 +39,216 @@ container.addEventListener('touchend', function (event) {
 });
 
 function showItem(index) {
-    // console.log(items)
-    const item = items[index];
 
-    const div = document.createElement('div');
+    try {
 
-    const title = document.createElement('div');   
-    title.innerHTML = `<h1>${item.title}</h1>`;
+        console.log(items)
+        items = items.sort((a, b) => new Date(b.published) - new Date(a.published));
+        const item = items[index];
 
-    let snippetText =  item["contentSnippet"] ? item["contentSnippet"] : ``;
+        const div = document.createElement('div');
 
-    const snippet = document.createElement('div');   
-    snippet.innerHTML = `<p>${snippetText}</p>`;
+        const title = document.createElement('div');
+        title.innerHTML = `<h1 class="fw-bold">${item.title}</h1>`;
 
-    let contentText =  item["content"] ? item["content"] : ``;
+        // let snippetText = item["contentSnippet"] ? item["contentSnippet"] : ``;
 
-    const content = document.createElement('div');   
-    content.innerHTML = `<p class="mt-4">${contentText}</p>`;
+        // const snippet = document.createElement('div');
+        // snippet.innerHTML = `<p>${snippetText}</p>`;
 
-    const link = document.createElement('p');   
-    link.innerHTML = `source: <a href="${item.link}" target="_blank">${new URL(item.link).hostname}</a>`;
-    link.className = 'small text-muted';
+        let contentText = item.content ? item.content : ``;
 
-    // div.appendChild(img);            
-    div.appendChild(title);
-    // div.appendChild(snippet);
-    div.appendChild(content);
-    div.appendChild(link);
-    
-    // div.appendChild(date);          
-    container.innerHTML = '';
-    container.appendChild(div);
+        const content = document.createElement('div');
+        content.innerHTML = `<p class="mt-4">${contentText}</p>`;
+
+        const imgsrc = document.createElement('img');        
+        imgsrc.setAttribute('src', item.visual);
+        imgsrc.setAttribute('alt', 'image');
+
+        // const favicon = document.createElement('img');        
+        // favicon.setAttribute('src', `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${new URL(item.canonicalUrl).hostname}&size=32`);
+        // favicon.setAttribute('alt', new URL(item.canonicalUrl).hostname);
+
+
+        const link = document.createElement('p');
+        let url = item.canonicalUrl ? item.canonicalUrl : ``;
+        let hostname = "";
+        try{
+            hostname =  new URL(url).hostname
+        }catch(err){
+            hostname=""
+        }       
+        link.innerHTML = `<a href="${url}" target="_blank">${hostname}</a>`;
+        link.className = 'small text-muted';
+
+        console.log(hostname, item.published)
+
+        // div.appendChild(img);  
+        // div.appendChild(favicon);          
+        div.appendChild(title);
+        div.appendChild(imgsrc);
+        // div.appendChild(snippet);
+        div.appendChild(content);
+        div.appendChild(link);
+
+        // div.appendChild(date);          
+        container.innerHTML = '';
+        container.appendChild(div);
+    } catch (err) {
+        console.log(err.stack);
+    }
+
 
 }
-getData(home_urls);
-$("#Home").on("click",function(){
-    getData(home_urls);
-})
-$("#Business").on("click",function(){
-    getData(business_urls);
-})
-  
-$("#Tech").on("click",function(){
-    getData(tech_urls);
-})
-  
-$("#Bollywood").on("click",function(){
-    getData(bollywood_urls);
-})
-  
-$("#cricket").on("click",function(){
-    getData(cricket_urls);
-})
-  
+
+
+// function getData(urls){
+//     items = [];
+//     Promise.all(
+//         urls.map(url => rss("https://winter-smoke-73a6.sixyjntpqun7805.workers.dev/?url=" + url).catch(error => ({ error })))
+//       )
+//         .then(results => {
+//           results.forEach(result => {
+//             if (result.error) {
+//               console.error(`Error fetching RSS feed: ${result.error.message}`);
+//               return;
+//             }
+//             items = [...items, ...result.items];
+//             items.sort((a, b) => new Date(b.isoDate) - new Date(a.isoDate));
+//           });
+//           showItem(0);
+//         })
+//         .catch(error => {
+//           console.error(`Error fetching RSS feeds: ${error.message}`);
+//         });
+
+// }
 
 var home_urls = [
-    `http://feeds.bbci.co.uk/news/rss.xml?edition=int`,
-    `http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml`,
-    `http://rss.cnn.com/rss/cnn_topstories.rss`,
-    `http://www.npr.org/rss/rss.php%3Fid%3D1001`,
-    `http://www.guardian.co.uk/rssfeed/0%2C%2C1%2C00.xml`,
-    `http://rss.time.com/web/time/rss/top/index.xml`,
-    `http://feeds.abcnews.com/abcnews/topstories`,
+    // `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds.abcnews.com/abcnews/topstories&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683049962262&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.npr.org/rss/rss.php?&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683005628303&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.guardian.co.uk/rssfeed/0,,1,00.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683050154579&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683050396951&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds.bbci.co.uk/news/world/rss.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052577057&ct=feedly.desktop&cv=31.0.1792`,
+]
+
+var india_urls = [
+
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.dnaindia.com/syndication/rss,catID-0.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683051862305&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds.feedburner.com/ndtvnews-top-stories&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683051931281&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://timesofindia.indiatimes.com/rssfeedstopstories.cms&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683051980576&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.ibnlive.com/rss/india.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052014746&ct=feedly.desktop&cv=31.0.1792`,
+
+]
+
+var business_urls = [
+
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds2.feedburner.com/businessinsider&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052419019&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds.feedburner.com/entrepreneur/latest&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052454625&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds.feedburner.com/fastcompany/headlines&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052488861&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://fortune.com/feed/&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052520611&ct=feedly.desktop&cv=31.0.1792`,
 
 ]
 
 var tech_urls = [
-    `http://www.theverge.com/rss/full.xml`,
-    `http://feeds2.feedburner.com/thenextweb`,
-    `http://www.engadget.com/rss-full.xml`,
-    `http://feeds.feedburner.com/Techcrunch`,
-    `http://feeds.gawker.com/lifehacker/vip`,
-    `http://feeds.wired.com/wired/index`,
-    `http://feeds.mashable.com/Mashable`,
-
-]
-
-
-var business_urls = [
-    `http://feeds2.feedburner.com/businessinsider`,
-    `http://feeds.harvardbusiness.org/harvardbusiness/`,
-    `http://feeds.feedburner.com/entrepreneur/latest`,
-    `http://feeds.feedburner.com/fastcompany/headlines`,
-    `http://feeds.feedburner.com/AtlanticBusinessChannel`,
-    `http://www.inc.com/rss.xml`,
-    `http://venturebeat.com/feed/`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.engadget.com/rss-full.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052780456&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.fromquarkstoquasars.com/feed/&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052813934&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds.gawker.com/gizmodo/full&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052837882&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://feeds.feedburner.com/Techcrunch&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052856972&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.theverge.com/rss/full.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683052880649&ct=feedly.desktop&cv=31.0.1792`,
 
 ]
 
 var bollywood_urls = [
-    `http://www.bollywoodlife.com/feed/`,
-    `http://www.filmfare.com/feeds/feeds.xml`,
-    `http://feeds.feedburner.com/missmalini`,
-    `http://www.pinkvilla.com/rss.xml`,
-    `http://www.rediff.com/rss/moviesrss.xml`,
-    `http://www.koimoi.com/feed/`,
-    `http://www.filmibeat.com/rss/filmibeat-bollywood-fb.xml`,
-
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.bollywoodlife.com/feed/&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683053032491&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.filmfare.com/feeds/feeds.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683053054080&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.koimoi.com/feed/&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683053079939&ct=feedly.desktop&cv=31.0.1792`,
+    `https://api.feedly.com/v3/streams/contents?streamId=feed/http://www.pinkvilla.com/rss.xml&count=20&unreadOnly=true&ranked=newest&similar=true&findUrlDuplicates=true&ck=1683053103299&ct=feedly.desktop&cv=31.0.1792`,
+   
 ]
 
-var cricket_urls = [
-    `http://www.cricinfo.com/rss/content/story/feeds/0.xml`,
-    `http://newsrss.bbc.co.uk/rss/sportonline_uk_edition/cricket/rss.xml`,
-    `http://www.guardian.co.uk/sport/cricket/rss`,
-    `http://www.rediff.com/rss/cricketrss.xml`,
-    `https://www.wisden.com/feed`,
 
-]
 
-function getData(urls){
+getData(home_urls);
+
+// function getData(urls) {
+//     items = [];
+//     Promise.all(
+//         urls.map(url => fetch("https://winter-smoke-73a6.sixyjntpqun7805.workers.dev/?url=" + url).catch(error => ({ error })))
+//     )
+//         .then(results => {
+//             // console.log(results.json());
+//             results.forEach(result => {
+//                 if (result.error) {
+//                     console.error(`Error fetching RSS feed: ${result.error.message}`);
+//                     return;
+//                 }
+//                 result.json().then((data) => {
+//                     // console.log(data.items)
+//                     items = [...items, ...data.items];
+//                     items = items.sort((a, b) => new Date(b.published) - new Date(a.published));
+//                     showItem(0);
+//                 });
+
+//             })
+//         })
+//         .catch(error => {
+//             console.error(`Error fetching RSS feeds: ${error.message}`);
+//         });
+
+// }
+
+async function getData(urls) {
     items = [];
-    Promise.all(
-        urls.map(url => rss("https://winter-smoke-73a6.sixyjntpqun7805.workers.dev/?url=" + url).catch(error => ({ error })))
-      )
-        .then(results => {
-          results.forEach(result => {
-            if (result.error) {
-              console.error(`Error fetching RSS feed: ${result.error.message}`);
-              return;
+    try {
+        const responses = await Promise.all(urls.map(url => fetch(`https://winter-smoke-73a6.sixyjntpqun7805.workers.dev/?url=${url}`).catch(error => ({ error }))));
+        for (const response of responses) {
+            if (response.error) {
+                console.error(`Error fetching RSS feed: ${response.error.message}`);
+                continue;
             }
-            items = [...items, ...result.items];
-            items.sort((a, b) => new Date(b.isoDate) - new Date(a.isoDate));
-          });
-          showItem(0);
-        })
-        .catch(error => {
-          console.error(`Error fetching RSS feeds: ${error.message}`);
-        });
-    
+            const data = await response.json();
+            const filteredItems = data.items.map(item =>
+            ({
+                title: item.title ? item.title : ``,
+                content: item.summary ? item.summary.content : ``,
+                published: item.published,
+                canonicalUrl: item.originId ? item.originId : ``,
+                visual: item.visual ? item.visual.url : `wp.png`
+                // add additional properties you want to extract here
+            })
+            );
+            items.push(...filteredItems);
+        }
+        items = items.sort((a, b) => new Date(b.published) - new Date(a.published));
+        // console.log(items);
+        showItem(0);
+    } catch (error) {
+        console.error(`Error fetching RSS feeds: ${error.message}`);
+    }
 }
+
+$("#Home").on("click", function () {
+    getData(home_urls);
+})
+$("#India").on("click", function () {
+    getData(india_urls);
+})
+$("#Business").on("click", function () {
+    getData(business_urls);
+})
+
+$("#Tech").on("click", function () {
+    getData(tech_urls);
+})
+
+$("#Bollywood").on("click", function () {
+    getData(bollywood_urls);
+})
+
+$("#cricket").on("click", function () {
+    getData(cricket_urls);
+})
 
 
 
